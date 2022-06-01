f6d9450092ec60bdb0178a54a03c748b80b8b454
Sanjarbek7777 <rasulovsanjarbek7777@gmail.com>
2022-06-01 10:31:12 +0500

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <v-card max-width="400px" class="mx-auto mt-5">
+      <v-toolbar color="success">
+        <v-toolbar-title> Input card </v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-text-field label="Main input" outlined dense></v-text-field>
+        <v-text-field label="Another input" outlined dense></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="success" dark>Submit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
